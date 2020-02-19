dd5f34b26923f793beaac179642f7bd2f54467d4
Chris Lankford <christopherlankford@gmail.com>
2020-02-19 14:13:35 -0800

    Basic game of life implementation

--- src/lib.rs
+++ src/lib.rs
@@ -49,12 +49,13 @@ impl Universe {
     }
 }
 
+// Gives Universe an implementaton of .to_string()
 impl fmt::Display for Universe {
     fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
         for line in self.cells.as_slice().chunks(self.width as usize) {
             for &cell in line {
                 let symbol = if cell == Cell::Dead { '◻' } else { '◼' };
-                write!(f, "{}", symbol)?;
+                write!(f, " {}", symbol)?;
             }
             write!(f, "\n")?;
         }
@@ -97,5 +98,31 @@ impl Universe {
         }
 
         self.cells = next;
+    }
+
+    // Constructor for a new Universe
+    pub fn new() -> Universe {
+        let width = 64;
+        let height = 64;
+
+        let cells = (0..width * height)
+            .map(|i| {
+                if i % 2 == 0 || i % 7 == 0 {
+                    Cell::Alive
+                } else {
+                    Cell::Dead
+                }
+            })
+            .collect();
+        
+        Universe {
+            width,
+            height,
+            cells
+        }
+    }
+
+    pub fn render(&self) -> String {
+        self.to_string()
     }
 }
