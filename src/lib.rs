168efa54afb125efc90b551ac9f68a670745f8eb
Chris Lankford <christopherlankford@gmail.com>
2020-02-21 23:21:48 -0800

    Added test code for WASM

--- src/lib.rs
+++ src/lib.rs
@@ -26,6 +26,7 @@ pub struct Universe {
     cells: Vec<Cell>
 }
 
+// Methods not being exported to Javascript
 impl Universe {
     fn get_index(&self, row: u32, column: u32) ->  usize {
         (row * self.width + column) as usize
@@ -46,6 +47,20 @@ impl Universe {
             }
         }
         count
+    }
+
+    // Get the dead and alive values of the entire universe
+    pub fn get_cells(&self) -> &[Cell] {
+        &self.cells
+    }
+
+    // Set cells to be alive in a universe by passing the row and column 
+    // of each cell as an array.
+    pub fn set_cells(&mut self, cells: &[(u32, u32)]) {
+        for (row, col) in cells.iter().cloned() {
+            let idx = self.get_index(row, col);
+            self.cells[idx] = Cell::Alive;
+        }
     }
 }
 
@@ -132,6 +147,18 @@ impl Universe {
 
     pub fn cells(&self) -> *const Cell {
         self.cells.as_ptr()
+    }
+
+    // Sets the width of the universe and resets all cells to the dead state.
+    pub fn set_width(&mut self, width: u32) {
+        self.width = width;
+        self.cells = (0..width * self.height).map(|_i| Cell::Dead).collect();
+    }
+
+    // Sets the height of the universe and resets all cells to the dead state.
+    pub fn set_height(&mut self, height: u32) {
+        self.height = height;
+        self.cells = (0..self.width * height).map(|_i| Cell::Dead).collect();
     }
 
     pub fn render(&self) -> String {
